bde3a868f1067f68339346a39d9903844012ff8c
Jeff Harris <jeffrey.harris@wwfus.org>
2014-10-26 12:24:39 -0400

    launch new site

--- css/config.rb
+++ css/config.rb
@@ -30,7 +30,7 @@ preferred_syntax = :scss
 css_dir = "stylesheets" # by Compass.app 
 sass_dir = "sass" # by Compass.app 
 images_dir = "images" # by Compass.app 
-output_style = :compressed # by Compass.app 
+output_style = :expanded # by Compass.app 
 relative_assets = false # by Compass.app 
 line_comments = false # by Compass.app 
 sass_options = {:debug_info=>false} # by Compass.app 
