407bd2d70a4be0df04ac401e4f19052ed8ad00d9
Jeff Harris <jeffrey.harris@wwfus.org>
2015-01-29 22:36:54 -0500

    adding files and alts

--- scripts/application-expand.js
+++ scripts/application-expand.js
@@ -109,6 +109,33 @@ $(window).scroll(function() {
 	});  
 });  
 
+$('.galleryPhotos').hide();
+$('.closePhotos').hide();
+
+
+morePhotos = function(e) {
+	e.preventDefault();
+
+	var $el = $(e.target)
+	var photos= $el.closest('.galleryBox').find('.galleryPhotos');
+	var openLink = $el.closest('.galleryBox').find('.openPhotos');
+	var closeLink = $el.closest('.galleryBox').find('.closePhotos');
+	
+	if (photos.is(":visible")){
+			photos.slideUp();
+			openLink.text("View Photos");
+			closeLink.hide();
+	}else{
+			photos.slideDown();
+			$el.text("Close");
+			closeLink.show();
+		}	
+}
+
+$(document).ready(function() {
+		$('.morePhotos').bind('click', morePhotos);
+});
+
 $(document).ready(function() {  
     /* Every time the window is scrolled ... */
     $(window).scroll( function(){
